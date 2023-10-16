0cbf9387a006fa29ca0cc1ec747fdc33a635f682
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-16 17:59:46 +0200

    feat/fix: database names and print on screen!

--- database/migrations/2023_10_16_141031_create_trains_table.php
+++ database/migrations/2023_10_16_141031_create_trains_table.php
@@ -15,15 +15,15 @@ return new class extends Migration {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('azienda', 50);
-            $table->enum('stazione_di_partenza', ['Torino Porta Nuova', 'Torino Porta Susa', 'Torino Lingotto', 'ecc..']);
-            $table->enum('stazione_di_arrivo', ['Milano Centro', 'Milano Rho', 'ecc..']);
-            $table->string('orario_di_partenza');
-            $table->string('orario_di_arrivo');
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
             $table->string('codice_treno', 25);
-            $table->float('numero_carrozze');
-            $table->string('in_orario', 50);
-            $table->string('cancellato', 50);
-            $table->float('prezzo_biglietto', 5, 2);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->float('prezzo_biglietto', 8, 2);
             $table->string('codice_biglietto', 50);
         });
     }
--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.app')
 
 @section('main-content')
-  <section class="container mt-5">
-    <div class="row">
+  <section class="container mt-5 mb-5">
+    <div class="row g-4">
         @foreach ($trains as $train)
-        <div class="card" style="width: 18rem;">
+        <div class="card mx-2 my-2" style="width: 18rem;">
+            <div class="card-header">
+                Treno 🚂
+              </div>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">{{$train->Azienda}}</li>
-              <li class="list-group-item">{{$train->}}</li>
-              <li class="list-group-item">{{$train->Azienda}}</li>
+              <li class="list-group-item"><strong>Azienda: </strong>{{$train->azienda}}</li>
+              <li class="list-group-item"><strong>Staz. di partenza: </strong>{{$train->stazione_di_partenza}}</li>
+              <li class="list-group-item"><strong>Staz. di arrivo: </strong>{{$train->stazione_di_arrivo}}</li>
+              <li class="list-group-item"><strong>Partenza 🕖: </strong>{{$train->orario_di_partenza}}</li>
+              <li class="list-group-item"><strong>Arrivo 🕖: </strong>{{$train->orario_di_arrivo}}</li>
+              <li class="list-group-item"><strong>Codice treno: </strong>{{$train->codice_treno}}</li>
+              <li class="list-group-item"><strong>Numero Carrozze: </strong>{{$train->numero_carrozze}}</li>
+              <li class="list-group-item"><strong>In orario: </strong>{{$train->in_orario}}</li>
+              <li class="list-group-item"><strong>Cancellato: </strong>{{$train->cancellato}}</li>
+              <li class="list-group-item"><strong>Prezzo biglietto: </strong>{{$train->prezzo_biglietto}}</li>
+              <li class="list-group-item"><strong>Codice biglietto: </strong>{{$train->codice_biglietto}}</li>
             </ul>
         </div>
         @endforeach
